68718c3671f8180ed299acfe9ad6a212b95f10b3
NSimpsonsK <github@9542.de>
2022-03-30 23:33:27 +0200

    add EB

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'udagram.cxt59vy0ewmo.us-east-1.rds.amazonaws.com:8080/api/v0'
 };
 
 /*
